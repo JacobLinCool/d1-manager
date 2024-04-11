6d89c4218f3ff6cce23d3bae6d316d92f5b1a13f
Mohammed Alamri <modev32@gmail.com>
2024-04-12 02:16:56 +0300

    List `special characters`

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,1 +1,4 @@
 // This file contains utility functions that are used in the project
+
+// This list contains all special characters that should be quoted in a name string
+const SPECIAL_CHARS = ["-"];
