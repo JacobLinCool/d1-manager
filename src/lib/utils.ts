ed3fb9961909a19f5bb5fa5f86c9758f775cdb6b
Mohammed Alamri <modev32@gmail.com>
2024-04-12 02:24:29 +0300

    Add utility function to iterate over records' keys to enclose strings with special chars

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -20,3 +20,16 @@ function should_quote(str: string) {
 export function quote_name(name: string): string {
 	return should_quote(name) ? `'${name}'` : name;
 }
+
+/**
+ * This function encloses all keys in a record in single quotes if they contain special characters
+ * @param record The record to quote
+ * @returns A new record with all keys enclosed in single quotes if they contain special characters
+ */
+export function quote_record(record: Record<string, any>): Record<string, any> {
+	return Object.fromEntries(
+		Object.entries(record).map(([key, value]) => {
+			return [quote_name(key), value];
+		}),
+	);
+}
